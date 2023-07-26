0653e464b74632e388bf1d07cbe2fb62211cf3ba
Yair Levy <yairlevyy5@gmail.com>
2023-07-26 12:20:37 +0200

    sm

---
+++ Week3/Day4/DailyChallenge/script.js
@@ -0,0 +1,11 @@
+let sentence = "The movie is not that bad, I like it!";
+let wordNot = sentence.indexOf("not");
+let wordBad = sentence.indexOf("bad");
+
+console.log(wordNot, wordBad);
+
+if (wordBad > wordNot) {
+    console.log(sentence.replace(sentence.substring(wordNot, wordBad + 3), "good"))
+} else {
+    console.log(sentence)
+}
