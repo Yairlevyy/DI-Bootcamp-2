edde09e3a4a2f05455cb1cbd8c703b133f44f99b
Yair Levy <yairlevyy5@gmail.com>
2023-08-10 22:30:28 +0200

    fm

---
+++ Week5/Day2/DailyChallenge/script.js
@@ -0,0 +1,24 @@
+// let form = document.forms[0];
+
+// let noun = form.noun;
+// let adjective = form.adjective;
+// let person = form.person;
+// let verb = form.verb;
+// let place = form.place;
+
+// let button = document.querySelector('#lib-button');
+
+// button.addEventListener('click', generateStory);
+
+// let story = document.querySelector('#story')
+
+// function generateStory(e) {
+//     e.preventDefault();
+//     nounValue = noun.value;
+//     adjectiveValue = adjective.value;
+//     personValue = person.value;
+//     verbValue = verb.value;
+//     placeValue = place.value;
+
+//     story.textContent = `Once upon a time, there was a ${adjectiveValue} ${nounValue} named ${personValue}. ${personValue} loved to ${verbValue} around the ${placeValue}. One day, while at the ${placeValue}, ${personValue} and the ${nounValue} found a hidden treasure. They were both so excited that they couldn't stop ${verbValue} and wagging their tails. Everyone at the ${placeValue} joined in the fun, and it turned into a day filled with laughter and joy.`;
+// };
