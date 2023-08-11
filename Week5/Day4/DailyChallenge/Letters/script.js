e6b212de97a9636acc753345930852c81606605e
Yair Levy <yairlevyy5@gmail.com>
2023-08-11 14:28:20 +0200

    sm

--- Week5/Day4/DailyChallenge/Letters/script.js
+++ Week5/Day4/DailyChallenge/Letters/script.js
@@ -1,16 +1,3 @@
-// function checkChar(e) {
-//     if(!e.key.match(/[a-zA-Z]/)){
-//         const newStr = deleteSpecs(e.target.value);
-//         // e.target.value = newStr;
-//         console.log(newStr)
-//     }
-// };
-
-// function deleteSpecs(str) {
-//     str.slice(0,-1);
-//     return str;
-// }
-
 const numberPattern = /^[0-9]+$/;
 const specialCharacterPattern = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 
