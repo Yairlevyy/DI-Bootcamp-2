f245413618409e46a9415f53053c1cdf55d14d43
Yair Levy <yairlevyy5@gmail.com>
2023-08-18 14:19:02 +0200

    'fm'

---
+++ Week6/Day2/DailyChallenge/daily_challenge.sql
@@ -0,0 +1,10 @@
+select count(*) from actor
+
+-- 200 Actors
+
+select * from actor
+
+insert into actor (first_name,last_name)
+values('Jhon','Doe')
+
+-- It's works, but since we didn't do the actor table in class i used this one, but i supposed that the one in the class had constraint not null and if it's the case that will return an error.
