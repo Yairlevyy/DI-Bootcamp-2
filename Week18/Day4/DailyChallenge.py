4c9055ab3abee72c24f3be6069904ead1a52f784
Yair Levy <yairlevyy5@gmail.com>
2023-12-08 14:10:42 +0100

    fm

---
+++ Week18/Day4/DailyChallenge.py
@@ -0,0 +1,21 @@
+# Challenge 1
+
+# num = int(input("Please, enter a number: "))
+# length = int(input("Please, enter a length: "))
+
+# num_list = [num*i for i in range(1,length+1) ]
+
+# print(num_list)
+
+# Challenge 2
+
+# user_str = input('Plase enter a str: ')
+# user_str_unique = []
+
+# for letter in user_str:
+#     if letter not in user_str_unique:
+#         user_str_unique.append(letter)
+
+# new_str = ''.join(user_str_unique)
+
+# print(new_str)
