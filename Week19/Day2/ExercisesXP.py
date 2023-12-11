1a1deaecf1269282e31dfd1359e9075cf46f1285
Yair Levy <yairlevyy5@gmail.com>
2023-12-11 16:13:42 +0100

    fm

---
+++ Week19/Day2/ExercisesXP.py
@@ -0,0 +1,70 @@
+# Exercise 1
+
+# def display_message():
+#     print('We are learning Pyhton in this course!')
+
+# display_message()
+
+# Exercise 2
+
+# def favorite_book(title):
+#     print(f"One of my favorite book is {title}")
+
+# favorite_book("Alice in Wonderland")
+
+# Exercise 3
+
+# def describe_city(city,country="Israel"):
+#     print(f"{city} is in {country}")
+    
+# describe_city("Tel Aviv","Israel")
+
+# Exercise 4
+
+# import random
+
+# def checkNumber(num):
+#     random_number = random.randint(1,100)
+#     if num == random_number:
+#         print("Bravo! You guess right!")
+#     else:
+#         print(f"Sorry, it's wrong. You choose {num} and the correct number was {random_number}")
+
+# checkNumber(20)
+
+# Exercise 5
+
+# def make_shirt(size="Large",message="I love Python"):
+#     print(f"The size of the shirt is {size} and the text is {message}")
+
+# make_shirt("Small","We stand with Israel and you should too!")
+
+# make_shirt()
+# make_shirt("Medium")
+# make_shirt("Extra-Large","I love Israel")
+# make_shirt(size="Extra-Large",message="I love Israel")
+
+# Exercise 6
+
+# magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+
+# def show_magicians(list):
+#     for item in list:
+#         print(item)
+
+# show_magicians(magician_names)
+
+# def make_great(list):
+#     for index, item in enumerate(list):
+#         list[index] = f"The great {item}"
+
+# make_great(magician_names)
+# show_magicians(magician_names)
+
+
+
+
+
+
+
+
